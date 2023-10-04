a45252017b6845e4a2162336f888b4dd2f88ceb9
bes-germes <55259601+bes-germes@users.noreply.github.com>
2023-10-04 05:27:47 +0300

    Add files via upload

---
+++ lab1/main.py
@@ -0,0 +1,136 @@
+import copy
+import time
+import functools
+
+
+##############lensort##################################
+def lensort_without_sort(list_of_words: list):
+    new_list_of_words = copy.deepcopy(list_of_words)
+
+    if type(new_list_of_words) is list:
+        for word in new_list_of_words:
+            if type(word) is str:
+                continue
+            else:
+                return "only for str"
+
+        for i in range(len(new_list_of_words) - 1):
+            for j in range(len(new_list_of_words) - i - 1):
+                if len(new_list_of_words[j]) > len(new_list_of_words[j + 1]):
+                    new_list_of_words[j], new_list_of_words[j + 1] = (
+                        new_list_of_words[j + 1],
+                        new_list_of_words[j],
+                    )
+
+        return new_list_of_words
+    else:
+        return "Wrong arg"
+
+
+def lensort_with_sorted(list_of_words: list):
+    return sorted(list_of_words)
+
+
+def lensort_with_sort(list_of_words: list):
+    new_list_of_words = copy.deepcopy(list_of_words)
+    new_list_of_words.sort()
+    return new_list_of_words
+
+
+def lensort_with_lambda_and_sort(list_of_words: list):
+    new_list_of_words = copy.deepcopy(list_of_words)
+    new_list_of_words.sort(key=lambda x: len(x))
+    return new_list_of_words
+
+
+def lensort_with_lambda_and_sorted(list_of_words: list):
+    return sorted(list_of_words, key=lambda x: len(x))
+
+
+########unique################
+
+
+def unique(list_of_something: list):
+    new_set = set(list_of_something)
+    new_list = []
+    for i in new_set:
+        new_list.append(i)
+    return new_list
+
+
+#########my_enumerate##################
+
+
+def my_enumerate(list_of_something: list):
+    new_list_idx = []
+    for i in range(len(list_of_something)):
+        new_list_idx.append(i)
+    list_zip = list(zip(new_list_idx, list_of_something))
+    return list_zip
+
+
+def input_txt_and_count_words(file_name: str):
+    d = {}
+
+    with open(file_name, "r", encoding="utf-8") as f:
+        text = f.readlines()
+
+    # return text
+    for line in text:
+        for word in line.split():
+            if word in d.keys():
+                d[word] += 1
+            else:
+                d[word] = 1
+    return d
+
+
+def timer(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        runtime = time.perf_counter() - start
+        print(f"{func.__name__} took {runtime:.10f} secs")
+        return result
+
+    return _wrapper
+
+
+@timer
+def to_power_numbers(list_of_numbers: list):
+    j = 0
+    for i in list_of_numbers:
+        list_of_numbers[j] *= i
+        j += 1
+    return list_of_numbers
+
+
+@timer
+def list_comprehension(list_of_numbers: list):
+    squares = [n * n for n in list_of_numbers]
+    return squares
+
+
+@timer
+def map_power(list_of_numbers: list):
+    return list(map(lambda x: x * x, list_of_numbers))
+
+
+# l = ["python", "perl", "java", "c", "haskell", "ruby"]
+# print(lensort_with_lambda_and_sorted(l))
+# print(l)
+
+# l = [1, 2, 1, 3, 2, 5]
+# print(unique(l))
+# print(l)
+
+# l = ["a", "b", "c"]
+# print(my_enumerate(l))
+
+# print(input_txt_and_count_words("test.txt"))
+
+l = [1, 2, 3, 4, 5, 6]
+print(to_power_numbers(l))
+print(list_comprehension(l))
+print(map_power(l))
---
+++ lab1/storage.py
@@ -0,0 +1,45 @@
+import argparse
+import json
+
+
+def storage_func(key=None, value=None):
+    if value is None:
+        with open("storage.json", "r") as write_file:
+            result = json.load(write_file)
+            if key in result.keys():
+                [print(num, end=", ") for num in result[key]]
+            else:
+                return "No such key"
+    if key and value:
+        d = {}
+
+        with open("storage.json", "r+") as write_file:
+            try:
+                d = json.loads(write_file.read())
+
+            except json.decoder.JSONDecodeError:
+                print("File is empty")
+
+            if key in d.keys():
+                for d_value in d[key]:
+                    if d_value == value:
+                        return "value is already exist in this key"
+
+                d.setdefault(key, [])
+                d[key].append(value)
+
+            else:
+                d[key] = [value]
+
+        with open("storage.json", "w") as write_file:
+            json.dump(d, write_file)
+
+
+parser = argparse.ArgumentParser(description="storage script")
+
+parser.add_argument("--key", dest="key_name", type=str)
+parser.add_argument("--val", dest="value", type=str)
+
+args = parser.parse_args()
+
+print(storage_func(args.key_name, args.value))
