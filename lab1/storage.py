908b8a0adb8a904eaa882f8a33445cfd9746bda1
bes-germes <55259601+bes-germes@users.noreply.github.com>
2023-10-06 02:40:43 +0300

    refactor storage.py

--- lab1/storage.py
+++ lab1/storage.py
@@ -2,38 +2,47 @@ import argparse
 import json
 import os.path
 
+
 def storage_func(key=None, value=None):
+    d = {}
+
+    if not os.path.exists("storage.json"):
+        with open("storage.json", "w") as write_file:
+            json.dump(d, write_file)
+            print("file was made")
+            exit(1)
+
+    with open("storage.json", "r+") as write_file:
+        try:
+            d = json.loads(write_file.read())
+
+        except json.decoder.JSONDecodeError:
+            print("File is empty, added syntax")
+            json.dump(d, write_file)
+            exit(1)
+
+    if key is None:
+        return print("enter key plz")
     if value is None:
         with open("storage.json", "r") as write_file:
             result = json.load(write_file)
+            if len(result) <= 0:
+                return print(None)
             if key in result.keys():
-                print(*result[key], sep=", ") 
+                print(*result[key], sep=", ")
             else:
                 return "No such key"
     if key and value:
-        d = {}
+        if key in d.keys():
+            for d_value in d[key]:
+                if d_value == value:
+                    return print("value is already exist in this key")
 
-        if not os.path.exists("storage.json"):
-            with open("storage.json", "w") as write_file:
-                json.dump(d, write_file)
-            
-        with open("storage.json", "r+") as write_file:
-            try:
-                d = json.loads(write_file.read())
+            d.setdefault(key, [])
+            d[key].append(value)
 
-            except json.decoder.JSONDecodeError:
-                print("File is empty")
-
-            if key in d.keys():
-                for d_value in d[key]:
-                    if d_value == value:
-                        return "value is already exist in this key"
-
-                d.setdefault(key, [])
-                d[key].append(value)
-
-            else:
-                d[key] = [value]
+        else:
+            d[key] = [value]
 
         with open("storage.json", "w") as write_file:
             json.dump(d, write_file)
@@ -46,4 +55,4 @@ parser.add_argument("--val", dest="value", type=str)
 
 args = parser.parse_args()
 
-storage_func(args.key_name, args.value))
+storage_func(args.key_name, args.value)
